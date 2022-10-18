fa286671b0a13af0aafe506185b2ad8d48cf6a8a
michaelson92 <michaelsonaddo@gmail.com>
2022-10-17 21:08:51 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - all single digits of base 10 from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '10'; num++)
+	{
+
+		printf("%d", num);
+	}
+
+	printf("\n");
+	return (0);
+}
+
