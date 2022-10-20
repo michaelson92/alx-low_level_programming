c9e6af6f663cbd8e624896fdde8434005a43503a
michaelson92 <michaelsonaddo@gmail.com>
2022-10-19 20:49:18 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	-puthchar ('\n');
