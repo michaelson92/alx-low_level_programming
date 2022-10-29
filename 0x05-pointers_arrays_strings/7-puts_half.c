a9a9900ca8d07d3059e45a93afab0db216550ed3
michaelson92 <michaelsonaddo@gmail.com>
2022-10-28 17:34:25 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* puts_half- prints half of a string
+* @str: string input
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+
+	str -= (i / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
