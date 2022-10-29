be489bfdc6c331f2202c05daa8f4c4859f12a1a6
michaelson92 <michaelsonaddo@gmail.com>
2022-10-28 17:44:54 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+  * print_array- prints elements of array
+  *@a: int type pointer
+  *@n: int input
+  */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n;  x++)
+	{
+		printf("%d", a[x]);
+		if (t != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
